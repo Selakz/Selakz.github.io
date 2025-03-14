b3f3533cacc0319e076296b66be29e69efedbff4
Selakz <787276422@qq.com>
2025-03-14 13:07:11 +0800

    Site updated: 2025-03-14 13:07:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/24/hello-world/","2025/01/09/selenium-bot/","2025/01/28/takana-cubic-instruction/"];function toRandomPost(){
+var posts=["2024/12/24/hello-world/","2025/03/13/first-interview/","2025/01/09/selenium-bot/","2025/01/28/takana-cubic-instruction/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
