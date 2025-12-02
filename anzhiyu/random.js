396ff5523f0e6d2790070ec0c809617405278a8f
Selakz <787276422@qq.com>
2025-12-03 00:23:09 +0800

    Site updated: 2025-12-03 00:23:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/02/co-and-contravariant/","2025/09/19/takana-cubic-update-log/","2024/12/24/hello-world/","2025/03/13/first-interview/","2025/01/09/selenium-bot/","2025/01/28/takana-cubic-instruction/"];function toRandomPost(){
+var posts=["2025/12/02/co-and-contravariant/","2025/03/13/first-interview/","2024/12/24/hello-world/","2025/01/09/selenium-bot/","2025/09/19/takana-cubic-update-log/","2025/01/28/takana-cubic-instruction/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
